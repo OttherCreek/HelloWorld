efbb3926cdc0c7f0ceea02e2ebccf53f8565f493
Jonathan Otterbach <jonathan.otterbach@student.uni-siegen.de>
2022-08-02 17:42:46 +0200

    Add project files.

---
+++ HelloWorld/src/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Log.h"
+
+int main()
+{
+	for (int i=0;i<5;i++) 
+	{
+		Log("Hello World!");
+	
+	}
+
+	Log("==============");
+	int i = 0;
+	while (i < 5)
+	{
+		Log("Hello World");
+		i++;
+	}
+	std::cin.get();
+}
