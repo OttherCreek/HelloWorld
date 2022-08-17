55d876cf5a4928f9bf86c95971d356d627c6713f
Jonathan Otterbach <jonathan.otterbach@student.uni-siegen.de>
2022-08-17 23:18:11 +0200

    references tutorial

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -2,6 +2,11 @@
 
 #define LOG(x) std::cout << x << std::endl
 
+void Increment(int& value)
+{
+	value++;
+}
+
 int main()
 {
 	char* buffer = new char[8]; //asking for/allocationg 8 bytes of memory, because one char = 1 byte
@@ -10,5 +15,19 @@ int main()
 	char** ptr = &buffer; //get the address of the pointer
 
 	delete[] buffer; //dealocate the reserved memory
+	
+	int a = 5;
+	int b = 8;
+
+	int* ref = &a;
+	*ref = 2;
+	ref = &b;
+
+	LOG(a);
+
+	Increment(a);
+
+	LOG(a);
+
 	std::cin.get();
 }
