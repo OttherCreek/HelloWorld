6a32e176af8d205d7ebec43e6fb0ca1ce4238c0d
Jonathan Otterbach <jonathan.otterbach@student.uni-siegen.de>
2022-08-17 23:30:31 +0200

    added pointer/ref explanation

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -9,19 +9,9 @@ void Increment(int& value)
 
 int main()
 {
-	char* buffer = new char[8]; //asking for/allocationg 8 bytes of memory, because one char = 1 byte
-	memset(buffer, 0, 8); //write to the buffer
-
-	char** ptr = &buffer; //get the address of the pointer
-
-	delete[] buffer; //dealocate the reserved memory
-	
 	int a = 5;
-	int b = 8;
-
-	int* ref = &a;
-	*ref = 2;
-	ref = &b;
+	int* ref = &a;	//pointer always holds a memory address
+	*ref = 2;		//dereferencing the pointer (access the value that is written at the specific address)	
 
 	LOG(a);
 
