10961888540b6cb0a67dd5cc0cfd751b88e99766
Jonathan Otterbach <jonathan.otterbach@student.uni-siegen.de>
2022-09-05 15:58:52 +0200

    this keyword (I guess)

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
 #include <string>
 
-struct Vector2
-{
-	float x, y;
+class Entity;
+void printEntity(const Entity&);
 
-	Vector2(float x, float y)
-		:x(x),y(y){}
+class Entity {
+public:
+	int x, y;
 
-	Vector2 operator+(const Vector2& other) const {
-		return Vector2(x + other.x, y + other.y);
+	Entity(int x, int y) {
+		this->x = x;
+		this->y = y;
+
+		Entity& e = *this;
+
+		printEntity(*this);
 	}
 
-	Vector2 operator*(const Vector2& other) const {
-		return Vector2(x * other.x, y * other.y);
-	}
-
-	bool operator==(const Vector2& other) const {
-		return x == other.x && y == other.y;
+	int getX() const {
+		const Entity& e = *this;
 	}
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other) {
-	stream << other.x << ", " << other.y;
-	return stream;
+void printEntity(const Entity& e) {
+	std::cout << e.x << ", " << e.y << std::endl;
 }
 
 int main()
 {
-	Vector2 position(4.0f, 4.0f);
-	Vector2 speed(0.5f, 1.5f);
-	Vector2 powerup(1.1f, 1.1f);
-
-	Vector2 res = position + speed * powerup;
-
-	std::cout << res << std::endl;
+	Entity e(2, 3);
 
 	std::cin.get();
 }
